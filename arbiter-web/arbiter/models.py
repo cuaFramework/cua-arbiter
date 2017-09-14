67c4667a1ab3be845a88c9cea9bea6c40892addc
wohui <wohui@xxx.com>
2017-09-14 17:57:55 +0800

    1.add case_save表和case_version表 2.wholog加上头部跳转

--- arbiter-web/arbiter/models.py
+++ arbiter-web/arbiter/models.py
@@ -3,7 +3,7 @@ from django.contrib import  admin
 from django.db import models
 from django.contrib.auth.models import Group
 from mongoengine import *
-
+import django.utils.timezone as timezone
 
 # Create your models here.
 #用户模型
@@ -21,12 +21,27 @@ class Run_Log (Document):
     # begin_time= models.DateTimeField(auto_now_add=True)
     # run_time = models.IntegerField(null=True)
     # operator = models.CharField(max_length=20,null=True)
-#用户模型
+#运行日志信息模型
 class RunInfo (models.Model):
     log_id = models.UUIDField()
     case_name = models.TextField(max_length=200)
     run_time = models.DateTimeField()
     author = models.CharField(max_length=50)
+
+    result = models.CharField(max_length=50,default='waiting')
+    version = models.IntegerField(default='10000000')
+
+#用例版本号信息
+class Case_Version_Info(models.Model):
+    case_name = models.TextField(max_length=200)#用例名，.py文件
+    latest_version = models.IntegerField(default=10000000)#版本号 八位
+    update_time = models.DateTimeField(default=timezone.now())
+#运行记录
+class Case_Save_Info(models.Model):
+    case_name = models.TextField(max_length=200)#用例名+方法
+    version = models.IntegerField(default=10000000)
+    author = models.CharField(max_length=50)
+    save_time = models.DateTimeField(default=timezone.now())
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username','password')
