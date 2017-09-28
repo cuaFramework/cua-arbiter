218e8d5a176e01bd671d517c7a2fb7d5803e695c
shimine <shimine@test.com>
2017-09-28 18:39:09 +0800

    去jquery计划1

--- arbiter-web/arbiter/static/arbiter/js/login.js
+++ arbiter-web/arbiter/static/arbiter/js/login.js
@@ -2,64 +2,75 @@
  * Created by Administrator on 2017/8/6.
  */
 
-        var app = new Vue({
-            el: '#login-form',
-            data: {
-                message: ''
-            }
-        });
-
-
-function login() {
-
-    $.ajax({
-        type: "POST",
-        url: "api-token-auth",
-        data: $("#login-form").serialize(),
-        success: function (msg) {
-
-            if (msg.token) {
-                let storage = window.localStorage;
-                storage["token"] = msg.token;
-                fetch("./getUserDetail",
-                    {
-                        method: "POST",
-                        credentials: "same-origin",
-                        headers: {
-                            'Accept': 'application/json, text/plain, */*',
-                            'Content-Type': 'application/json'
-                        }
-                    }).then(response => {
-                    if(response.status !== 200
-            )
+var app = new Vue({
+    el: '#login-form',
+    data: {
+        inputtext: {},
+        prompt:""
+    },
+    methods: {
+        submit: function () {
+            fetch("api-token-auth",
                 {
-                    console.log("存在一个问题，状态码为：" + response.status);
+                    method: "POST",
+                    credentials: "same-origin",
+                    headers: {
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(this.inputtext)
+                }).then(function (response) {
+                if (response.status !== 200
+                ) {
+                   this.prompt="账号或密码错误";
                     return false;
                 }
-            else
-                return response.json();
+                else
+                    return response.json();
+            }).then(
+                function (msg) {
+                    if (msg.token) {
+                        let storage = window.localStorage;
+                        storage["token"] = msg.token;
+                        fetch("./getUserDetail",
+                            {
+                                method: "POST",
+                                credentials: "same-origin",
+                                headers: {
+                                    'Accept': 'application/json, text/plain, */*',
+                                    'Content-Type': 'application/json'
+                                }
+                            }).then(response => {
+                            if (response.status !== 200
+                            ) {
+                                console.log("存在一个问题，状态码为：" + response.status);
+                                return false;
+                            }
+                            else
+                                return response.json();
 
-            }).
-                then(
-                    json => {
-                    storage["username"] = json["username"];
-                storage["role"] = json["role"];
-                window.location.href = ".";
-            }
-            )
-                ;
+                        }).then(
+                            json => {
+                                storage["username"] = json["username"];
+                                storage["role"] = json["role"];
+                                window.location.href = ".";
+                            }
+                        );
 
-            }
-            else {
-                $("#LoginInfo").text("认证失败");
+                    }
+                    else {
+                       this.prompt="认证失败";
 
-            }
-        },
-        error: function () {
-            $("#LoginInfo").text("账号或密码错误");
+                    }
+                }
+            );
         }
-    });
-    return false;
-}
+    }
+
+});
+
+
+
+
 
 
