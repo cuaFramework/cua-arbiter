25220bcb1b5ad54d4be3f081ea5651b3ceab593e
wohui <wohui@xxx.com>
2017-11-27 20:23:05 +0800

    1.修改日志模块表格样式 2.刚刚提交的那个注释是手误，实际是提交了api-count页面

--- arbiter-web/arbiter/static/arbiter/js/wholog/index.js
+++ arbiter-web/arbiter/static/arbiter/js/wholog/index.js
@@ -6,7 +6,6 @@
 var log_app = new Vue({
     el: '#log-root',
     data: {
-        auto_ok:true,
         tableData: [
         {
           logId: '123455-22667728800-222-1128',
@@ -32,7 +31,90 @@ var log_app = new Vue({
           author:'hui',
           runTime:'2017-10-23'
         },
-
+            {
+          logId: '123455-22667728800-222-1128',
+          caseName: 'test-123',
+          author:'hui',
+          runTime:'2019-10-22T10:11:12 122'
+        },
+        {
+          logId: '74363673822',
+          caseName: 'yuyueu',
+          author:'hui',
+          runTime:'2018-10-22'
+        },
+        {
+          logId: 'Ju383838321',
+          caseName: 'ghyueueueueeyysususussuususuusuususjxjs',
+          author:'hui',
+          runTime:'2017-11-22'
+        },
+        {
+          logId: '9303-33-333h',
+          caseName: '干哈个和尚师父时间计算机决绝与诶',
+          author:'hui',
+          runTime:'2017-10-23'
+        },
+            {
+          logId: '123455-22667728800-222-1128',
+          caseName: 'test-123',
+          author:'hui',
+          runTime:'2019-10-22T10:11:12 122'
+        },
+        {
+          logId: '74363673822',
+          caseName: 'yuyueu',
+          author:'hui',
+          runTime:'2018-10-22'
+        },
+        {
+          logId: 'Ju383838321',
+          caseName: 'ghyueueueueeyysususussuususuusuususjxjs',
+          author:'hui',
+          runTime:'2017-11-22'
+        },
+        {
+          logId: '9303-33-333h',
+          caseName: '干哈个和尚师父时间计算机决绝与诶',
+          author:'hui',
+          runTime:'2017-10-23'
+        },
+{
+          logId: '74363673822',
+          caseName: 'yuyueu',
+          author:'hui',
+          runTime:'2018-10-22'
+        },
+        {
+          logId: 'Ju383838321',
+          caseName: 'ghyueueueueeyysususussuususuusuususjxjs',
+          author:'hui',
+          runTime:'2017-11-22'
+        },
+        {
+          logId: '9303-33-333h',
+          caseName: '干哈个和尚师父时间计算机决绝与诶',
+          author:'hui',
+          runTime:'2017-10-23'
+        },
+            {
+          logId: '74363673822',
+          caseName: 'yuyueu',
+          author:'hui',
+          runTime:'2018-10-22'
+        },
+        {
+          logId: 'Ju383838321',
+          caseName: 'ghyueueueueeyysususussuususuusuususjxjs',
+          author:'hui',
+          runTime:'2017-11-22'
+        },
+        {
+          logId: '9303-33-333h',
+          caseName: '干哈个和尚师父时间计算机决绝与诶',
+          author:'hui',
+          runTime:'2017-10-23'
+        },
       ],
     },
 
