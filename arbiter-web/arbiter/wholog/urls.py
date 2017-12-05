4152604f00dfa69584c61746d2c1c705dff06e21
wohui <wohui@xxx.com>
2017-12-05 18:13:02 +0800

    修改用例和日志模块的跳转链接，修改样式

--- arbiter-web/arbiter/wholog/urls.py
+++ arbiter-web/arbiter/wholog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
      url(r'^getAllLog', views.getAllLog, name='getAllLog'),
      url(r'^logDetail111', views.getDetailLog, name='getDeatailLog'),
      #todo 登录登出抽离出来，现在core和wholog下的view均有
-     url(r'^getUserDetail', views.auth_restful.get_user_detail),
+     url(r'^getUserDetail', views.auth_restful.get_user_detail,name='getUserDetail'),
      url(r'^queryLogData', views.queryLogData, name='queryLogData'),
      url(r'^home',views.home,name='home'),
      url(r'^index', views.index, name='index'),
