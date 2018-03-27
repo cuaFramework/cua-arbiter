0213e79877cde8a7cb8fd3d04f66d8d11d054e2d
shimine <shimine@test.com>
2018-03-27 09:15:10 +0800

    bug fix

--- arbiter-web/config.py
+++ arbiter-web/config.py
@@ -1,7 +1,7 @@
 arbiter_prod_config = \
     dict(redis_host='10.104.102.142',
          redis_port=6379,
-         redis_dj_db=12,
+         redis_dj_db=13,
          redis_arbiter_db=9,
          redis_elk_db=11,
          pgsql_host='10.104.104.39',
@@ -10,7 +10,7 @@ arbiter_prod_config = \
          pgsql_user='luna',
          pgsql_password='luna',
          elk_url='10.104.104.57:9200',
-         case_path='caseobj/casex')
+         case_path='caseobj/case')
 
 arbiter_docker_config = \
     dict(redis_host='redis',
@@ -24,4 +24,4 @@ arbiter_docker_config = \
          pgsql_dbname='arbiter_dj',
          pgsql_password='luna',
          elk_url='elk:9200',
-         case_path='caseobj/casex')
+         case_path='caseobj/case')
