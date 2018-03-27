39da31ace36c20b7cf47878f5c3b149ff764a402
shimine <shimine@test.com>
2018-03-27 17:35:30 +0800

    bug fix

--- arbiter-web/stack-fix.c
+++ arbiter-web/stack-fix.c
@@ -5,7 +5,6 @@
 typedef int (*func_t)(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg);
 
 int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine)(void*), void *arg) {
-	printf("arbiter pthread_create override called\n");
 
 	pthread_attr_t local;
 	int used = 0, ret;
